d1c993f5377eea342d26acaf4bf213f3023ee625
Alexander Küpper <adkupper@gmail.com>
2022-10-12 21:44:51 +0200

    Add help string

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,18 @@ int main (int argc, char *argv[]){
 
     if (argc < 2)
     {
-        std::cerr << "Usage: " << argv[0] << " [Keyboard device path]" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " [Keyboard device path] \n"
+            "\nPath to the keyboard device. When keyboard has n-key rollover, take care that the device name ends in 'kbd' else"
+            " it will only create events when more than six keys are pressed simultaneously"
+            "\n\nAs of now, only six-key rollover is implemented."
+            << std::endl;
+
         return -1;
     }
 
     std::string keyboard_device = argv[1];
 
     EventQueue event_queue;
-
 
     InputThread input_thread(keyboard_device, &event_queue);
     AudioThread audio_thread(&event_queue);
