4f35217d5ed6c9f9416222cd68dc9421194264c2
Alexander Küpper <adkupper@gmail.com>
2022-10-21 21:10:17 +0200

    Update formatting

--- src/waveform.cpp
+++ src/waveform.cpp
@@ -2,41 +2,36 @@
 #include <iostream>
 
 
-
-
-
-
-Waveform::Waveform(float _freq):
-                        norm_freq(_freq){
-
+Waveform::Waveform(float _freq)
+  : norm_freq(_freq)
+{
 }
-
-
-
 
 
 FrameBuffer& SineWave::generate(){
 
-  unsigned int n;
-  for (n = 0; n < FrameBuffer::frame_size; n++){
-    // TODO: this should probably be an approximation..
-    float sample = std::sin(3.14151598 * 2 * norm_freq * n + phase);
+    unsigned int n;
+    for (n = 0; n < FrameBuffer::frame_size; n++)
+    {
+        // TODO: this should probably be an approximation..
+        float sample = std::sin(3.14151598 * 2 * norm_freq * n + phase);
 
-    buffer[n] = sample; // TODO: fix interleaving
-    //buffer[2 * n + 1] = sample;
+        buffer[n] = sample; // TODO: fix interleaving
+                            //buffer[2 * n + 1] = sample;
+    }
 
-  }
+    phase = 3.141598 * 2 *norm_freq * n + phase;
 
-  phase = 3.141598 * 2 *norm_freq * n + phase;
-
-  return buffer;
+    return buffer;
 }
 
 
 FrameBuffer& SquareWave::generate(){
 
     for (std::size_t i = 0; i < FrameBuffer::frame_size; i++)
+    {
         buffer[i] = 0;
+    }
 
     return buffer;
 }
